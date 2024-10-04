5e91a5192f316879a49e2e790e8f1a48e3c26fcc
DushyanthaAT <dushyantha721@gmail.com>
2024-10-05 00:19:13 +0530

    Create Item2.jsx

---
+++ src/Components/Item2/Item2.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import "./Item2.css";
+const Item2 = () => {
+  return (
+    <div className="item item-2">
+      <div className="icons">
+        <MdOutlineWorkOutline />
+      </div>
+      <span className="txt1">Completed</span>
+      <span className="txt2">10 + Projects</span>
+    </div>
+  );
+};
+
+export default Item2;
