8ce8f6c34310d5cf908bcdcd465ba336b201aee8
DushyanthaAT <dushyantha721@gmail.com>
2024-10-05 00:19:18 +0530

    Create Item3.jsx

---
+++ src/Components/Item3/Item3.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Item3.css";
+
+const Item3 = () => {
+  return (
+    <div className="item item-3">
+      <span className="txt1">I am</span>
+      <span className="txt2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae
+        placeat veritatis quisquam facilis sunt neque? Aliquid, atque id.
+        Deleniti nulla dolorem sunt suscipit. Sint nemo pariatur exercitationem
+        ea eaque qui excepturi nesciunt fugiat quas natus laborum officiis vero
+        tempora neque sunt inventore, corrupti fugit quibusdam! Fuga porro
+        doloribus repellat pariatur voluptatibus? Non accusamus eligendi nihil
+        ipsam consequatur debitis facere architecto aliquid assumenda, quidem
+        repellendus, voluptatem necessitatibus, porro praesentium facilis?
+      </span>
+    </div>
+  );
+};
+
+export default Item3;
