f0b8262600d79e92ec49c15de813c71b22ab6a20
DushyanthaAT <dushyantha721@gmail.com>
2024-10-05 00:19:04 +0530

    Create Item1.jsx

---
+++ src/Components/Item1/Item1.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { TbDeviceImacStar } from "react-icons/tb";
+import "./Item1.css";
+
+const Item1 = () => {
+  return (
+    <div className="item item-1">
+      <div className="icons">
+        <TbDeviceImacStar />
+      </div>
+      <span className="txt1">Experience</span>
+      <span className="txt2">2 + Years</span>
+    </div>
+  );
+};
+
+export default Item1;
