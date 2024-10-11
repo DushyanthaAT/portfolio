657d538479d587c7507036bf8c249700286f42f2
DushyanthaAT <dushyantha721@gmail.com>
2024-10-12 00:24:17 +0530

    Update Item8.jsx

--- src/Components/Item8/Item8.jsx
+++ src/Components/Item8/Item8.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import "./Item8.css";
 import CardComponent from "./cardComponent/CardComponent";
 import {
@@ -25,6 +26,7 @@ import {
 const Item8 = () => {
   const cardData = [
     {
+      id: 1,
       iconImages: [html, css, js],
       imageurl: webSite,
       title: "TestTitle",
@@ -32,28 +34,96 @@ const Item8 = () => {
       beUrl: "https://www.behance.com",
       gitUrl: "https://www.github.com",
       figUrl: "https://www.figma.com",
-      Category: "web",
+      Category: "Mobile",
+    },
+    {
+      id: 2,
+      iconImages: [html, css, js],
+      imageurl: webSite,
+      title: "TestTitle",
+      desc: "Lorem ipsum dolor sit amet consectetur adipi",
+      beUrl: "https://www.behance.com",
+      gitUrl: "https://www.github.com",
+      figUrl: "https://www.figma.com",
+      Category: "Web",
+    },
+    {
+      id: 3,
+      iconImages: [html, css, js],
+      imageurl: webSite,
+      title: "TestTitle",
+      desc: "Lorem ipsum dolor sit amet consectetur adipi",
+      beUrl: "https://www.behance.com",
+      gitUrl: "https://www.github.com",
+      figUrl: "https://www.figma.com",
+      Category: "Web",
     },
   ];
+
+  const projectsNav = [
+    {
+      name: "All",
+    },
+    {
+      name: "Web",
+    },
+    {
+      name: "Mobile",
+    },
+    {
+      name: "UI/UX",
+    },
+    {
+      name: "Game",
+    },
+  ];
+
+  const [item, setItem] = useState({ name: "All" });
+  const [projects, setProjects] = useState([]);
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    if (item.name === "All") {
+      setProjects(cardData);
+    } else {
+      const newProjects = cardData.filter((project) => {
+        return project.Category === item.name;
+      });
+      setProjects(newProjects);
+    }
+  }, [item]);
+
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent });
+    setActive(index);
+  };
   return (
     <div className="item item-8">
       <div className="portfolio-container">
         <div className="portfolio-header">
           <div className="txt1">Portfolio</div>
         </div>
+        <div className="work_filter">
+          {projectsNav.map((item, index) => {
+            return (
+              <span
+                onClick={(e) => {
+                  handleClick(e, index);
+                }}
+                className={`${
+                  active === index ? "active-work" : ""
+                } port-category`}
+                key={index}
+              >
+                {item.name}
+              </span>
+            );
+          })}
+        </div>
         <div className="portfolio-items">
-          {cardData.map((data, index) => (
-            <CardComponent
-              key={index}
-              iconImages={data.iconImages}
-              ImgURL={data.imageurl}
-              Title={data.title}
-              Desc={data.desc}
-              BeURL={data.beUrl}
-              GitURL={data.gitUrl}
-              FigURL={data.figUrl}
-            />
-          ))}
+          {projects.map((item) => {
+            return <CardComponent item={item} key={item.id} />;
+          })}
         </div>
       </div>
     </div>
