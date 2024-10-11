6553a4a0c922a9ee68c227d63213178e511cb461
DushyanthaAT <dushyantha721@gmail.com>
2024-10-12 00:24:22 +0530

    Create Item9.jsx

---
+++ src/Components/Item9/Item9.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Image_Slider from "../Image_Slider/Image_Slider";
+import "./Item9.css";
+const Item9 = () => {
+  return (
+    <div className="item item-9">
+      <div className="winnings-header">
+        <div className="txt1">Winnings</div>
+      </div>
+      <div className="imgSlider">
+        <Image_Slider />
+      </div>
+    </div>
+  );
+};
+
+export default Item9;
