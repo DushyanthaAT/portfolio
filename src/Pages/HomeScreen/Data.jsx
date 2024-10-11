7d460d677ab5b2b3d12dd0f5db1a86a251f8d070
DushyanthaAT <dushyantha721@gmail.com>
2024-10-12 00:24:44 +0530

    Create Data.jsx

---
+++ src/Pages/HomeScreen/Data.jsx
@@ -0,0 +1,35 @@
+import {
+  profileImage,
+  boostrap,
+  c,
+  css,
+  figma,
+  firebase,
+  flutter,
+  git,
+  github,
+  html,
+  java,
+  js,
+  mongoDB,
+  mySQL,
+  nodeJs,
+  python,
+  react,
+  xd,
+  webSite,
+} from "../../images";
+const cardData = [
+  {
+    iconImages: [html, css, js],
+    imageurl: webSite,
+    title: "TestTitle",
+    desc: "Lorem ipsum dolor sit amet consectetur adipi",
+    beUrl: "https://www.behance.com",
+    gitUrl: "https://www.github.com",
+    figUrl: "https://www.figma.com",
+    Category: "web",
+  },
+];
+
+export default cardData;
