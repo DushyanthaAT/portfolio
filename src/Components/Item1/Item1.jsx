3e5a2c0375473eb67e528b9560258c833c184ec5
DushyanthaAT <dushyantha721@gmail.com>
2024-10-16 03:18:11 +0530

    Update Item1.jsx

--- src/Components/Item1/Item1.jsx
+++ src/Components/Item1/Item1.jsx
@@ -4,7 +4,7 @@ import "./Item1.css";
 
 const Item1 = () => {
   return (
-    <div className="item item-1">
+    <div className="item item-1in">
       <div className="icons">
         <TbDeviceImacStar />
       </div>
