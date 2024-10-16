99c67569659b55688a068e1aa0c3bcf88e75e608
DushyanthaAT <dushyantha721@gmail.com>
2024-10-16 23:36:44 +0530

    Update test.jsx

--- src/test.jsx
+++ src/test.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./test.css";
+import { MdOutlineFileDownload } from "react-icons/md";
+import Button from "@mui/material/Button";
 
 const Test1 = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -12,16 +14,30 @@ const Test1 = () => {
     });
   };
   return (
-    <div className="Wrapper">
-      <div
-        className="box"
-        onMouseMove={handleMouseMove}
-        style={{
-          "--x": `${mousePosition.x}px`,
-          "--y": `${mousePosition.y}px`,
-          "--clr": "#ffee84",
-        }}
-      ></div>
+    <div
+      className="box"
+      onMouseMove={handleMouseMove}
+      style={{
+        "--x": `${mousePosition.x}px`,
+        "--y": `${mousePosition.y}px`,
+        "--clr": "#ffee84",
+      }}
+    >
+      <div className="btn-cover">
+        <Button
+          variant="contained"
+          startIcon={<MdOutlineFileDownload />}
+          sx={[
+            { borderRadius: 50 },
+            { width: "conterntFit" },
+            { fontSize: "0.55rem" },
+            { padding: "2px 15px" },
+            { backgroundColor: "var(--appBlue)" },
+          ]}
+        >
+          Download
+        </Button>
+      </div>
     </div>
   );
 };
