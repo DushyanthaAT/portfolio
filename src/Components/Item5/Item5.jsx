76fc006b2642f3b3e30ffeeef15a2df4739f976d
DushyanthaAT <dushyantha721@gmail.com>
2024-10-19 19:39:10 +0530

    Update Item5.jsx

--- src/Components/Item5/Item5.jsx
+++ src/Components/Item5/Item5.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { IoDocumentTextOutline } from "react-icons/io5";
-import Button from "@mui/material/Button";
 import { MdOutlineFileDownload } from "react-icons/md";
 import "./Item5.css";
 import { HoverBorderGradient } from "../UI/hover-border-gradient";
@@ -12,30 +11,17 @@ const Item5 = () => {
         <IoDocumentTextOutline />
       </div>
       <div className="txt1">CV</div>
-      {/* <div className="btn-cont">
-        <Button
-          variant="contained"
-          startIcon={<MdOutlineFileDownload />}
-          sx={[
-            { borderRadius: 50 },
-            { width: "conterntFit" },
-            { fontSize: "0.55rem" },
-            { padding: "2px 15px" },
-            { backgroundColor: "var(--appBlue)" },
-          ]}
-        >
-          Download
-        </Button>
-      </div> */}
       <div className="btn-cont">
-        <HoverBorderGradient
-          containerClassName="rounded-full"
-          as="button"
-          className="HoverBorderGradient"
-        >
-          <MdOutlineFileDownload size={15} />
-          <span className="text">Download</span>
-        </HoverBorderGradient>
+        <a href="/cv.pdf" download="Dushyantha_CV.pdf" className="download-btn">
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="HoverBorderGradient"
+          >
+            <MdOutlineFileDownload size={15} />
+            <span className="text">Download</span>
+          </HoverBorderGradient>
+        </a>
       </div>
     </div>
   );
