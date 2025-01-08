7d2ac7dd289d9db573151e7e00e982c637bd6a6c
DushyanthaAT <dushyantha721@gmail.com>
2025-01-08 23:41:31 +0530

    Update Buttons.jsx

--- src/Pages/Buttons.jsx
+++ src/Pages/Buttons.jsx
@@ -6,7 +6,7 @@ const Buttons = (menuItems) => {
       {menuItems.map((val) => (
         <button className="btn1">{val}</button>
       ))}
-      <button className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold">
+      <button className="btn-dark text-red p-1 px-2 mx-5 btn fw-bold">
         All
       </button>
     </div>
